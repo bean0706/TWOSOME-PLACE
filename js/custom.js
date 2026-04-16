fdf4de83a1e9c0edd026f562cea5c6454866fbe0
bean0706 <luckydada1216@gmail.com>
2026-04-16 16:45:22 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,29 @@
+$(function () {
+
+
+
+
+    const swiper = new Swiper(" .main_visual_slide", {
+        loop: true, autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+
+        pagination: {
+            clickable: true,
+            el: '.main_visual .page',
+        },
+
+
+    });
+
+
+
+
+
+
+
+
+})
+
+
